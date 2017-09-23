27bea2756ee7b80e0286f04cf0f78bf3dbd6f543
Jens GEBHARDT <dt0a35@canda.com>
2017-09-24 00:30:58 +0200

    added create-link

--- src/App.js
+++ src/App.js
@@ -7,36 +7,43 @@ import CreateContact from './CreateContact'
 
 class App extends Component {
 
-  state ={
+  state = {
     screen: 'list',
     contacts: []
   }
 
-  componentDidMount(){
+  componentDidMount() {
     ContactsAPI.getAll().then((contacts) => {
-      this.setState({contacts})
+      this.setState({ contacts })
     })
   }
 
   removeContact = (contact) => {
     this.setState((state) => ({
-      contacts: state.contacts.filter((c)=> c.id !== contact.id)
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
 
     ContactsAPI.remove(contact)
+  }
+
+  navigateToCreate = () => {
+    this.setState({screen: 'create'})
   }
 
   render() {
     return (
       <div>
         {this.state.screen === 'list' && (
-        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
+          <ListContacts 
+          onDeleteContact={this.removeContact} 
+          contacts={this.state.contacts}
+          onNavigate={this.navigateToCreate} />
         )}
         {this.state.screen === 'create' && (
-        <CreateContact/>)}
+          <CreateContact />)}
       </div>
-    
-      
+
+
     )
   }
 }
